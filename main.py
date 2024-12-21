81055270b55e5a110a8a170c6520fdb1b77f98c8
Schroeder-R <Schroeder357@gmail.com>
2024-12-21 11:38:37 -0600

    Added FPS limit from Chapter 2 Lesson 3

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+import time
 
 def main():
     print("Starting asteroids!")
@@ -9,6 +10,11 @@ def main():
     # pygame initialization
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    frame_count = 0
+    start_time = time.time()
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +23,14 @@ def main():
 
         screen.fill((0, 0, 0))  # clear the screen
         pygame.display.flip()
+
+        frame_count += 1
+        if time.time() - start_time > 1:
+            print(f"FPS: {frame_count}")
+            frame_count = 0
+            start_time = time.time()
+
+        dt = clock.tick(144) / 1000.0  # limit the FPS to 60 and convert to seconds
 
 if __name__ == "__main__":
     main()
