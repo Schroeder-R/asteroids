429011c0ac10792d9e7ae661abb5c25f335ca5f5
Schroeder-R <Schroeder357@gmail.com>
2024-12-22 11:44:59 -0600

    added the asteroid.py and asteroidfield.py files and updated main.py to use them

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
 
@@ -9,13 +11,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     
-    updatable = pygame.sprite.Group()
+    updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
-    Player.containers = (updatable, drawable)
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable,)
 
     # game variables
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     
     dt = 0
 
@@ -24,7 +30,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        for obj in updatable:
+        for obj in updateable:
             obj.update(dt)
 
         screen.fill((0, 0, 0))  # clear the screen
